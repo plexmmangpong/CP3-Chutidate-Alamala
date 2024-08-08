5272d479e54df487d0cfc363c58cfaa623283543
Chutidate Alamala <51482571+plexmmangpong@users.noreply.github.com>
2024-08-08 09:46:38 +0700

    VatCaculator.py

--- VatCaculator.py
+++ VatCaculator.py
@@ -1,10 +1,7 @@
 def VatCaculator(totalprice):
     result = totalprice+(totalprice*7/100)
     return result
+    
 print("\t(VAT CACULATOR)")
 user = int(input("Enter Price : "))
 print("TotalPrice >>",VatCaculator(user))
-
-
- 
-    
