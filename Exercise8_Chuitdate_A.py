f7af54bdd5baa4232d3904fdf3df641887f4de5f
Chutidate Alamala <51482571+plexmmangpong@users.noreply.github.com>
2025-05-31 14:52:45 +0700

    Update Exercise8_Chuitdate_A.py

--- Exercise8_Chuitdate_A.py
+++ Exercise8_Chuitdate_A.py
@@ -3,8 +3,8 @@ password = input("Pasword : ")
 if username == "pleum" and password == "1234":
     print("Success")
     print(f"[---Wellcome {username} ---]")
-    print("1.Mac Pro M3\nPrice : 79,900 Baht")
-    print("2.Mac Air M3\nPrice : 39,900 Baht")
+    print("1.Mac Pro M3 : 79,900 ฿")
+    print("2.Mac Air M3 : 39,900 ฿")
     while True:
         select = int(input(">>"))
         if select == 1:
@@ -21,9 +21,11 @@ if username == "pleum" and password == "1234":
             print("Please Enter 1-2")
             continue
         
-    print(f"\n\n{product}\nprice : {price}")    
+    print(f"\n\n{product}\nprice : {price:,}฿")    
     amount = int(input("Enter amount you want : "))
-    print(f"\n\nProduct : {product}\nPrice : {price}\nAmount : {amount}\nTotalPrice : {amount*price}")  
+    
+    print(f"\n\n{product} x{amount:,}\n{amount*price:,}฿")
+    print("Succes!your order will be delivered in 3 days.") 
         
 else :
     print("Invalid email or password")
